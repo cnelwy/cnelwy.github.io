1d94387e13f5d3f423f7d9920fd3db2d9ba0766b
Lottie Elwy <cnelwy@gmail.com>
2024-12-08 13:01:56 -0500

    Update save_to_csv.php

--- save_to_csv.php
+++ save_to_csv.php
@@ -5,6 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $bathroom = htmlspecialchars($_POST["bathroom"]);
     $access = htmlspecialchars($_POST["access"]);
 
+    // displays the data in the terminal so that I can check if it works
     echo "<h1>Form Submission</h1>";
     echo "Building: $building<br>";
     echo "Gender: $gender<br>";
